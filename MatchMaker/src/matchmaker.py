8bf390b1a81b68869a12dbc12c59daf5366daf2f
ord <mailordahan@gmail.com>
2013-12-22 20:33:16 +0200

    Implemented the basic genetic algorithm scheme

--- MatchMaker/src/matchmaker.py
+++ MatchMaker/src/matchmaker.py
@@ -5,6 +5,7 @@ Created on Dec 22, 2013
 '''
 from solution import Solution
 import time
+from random import random, choice
 
 
 class MatchMaker(object):
@@ -12,24 +13,33 @@ class MatchMaker(object):
     Simulates match-making using genetic algorithms
     '''
 
-    def __init__(self, categories, items_per_category, generation_size=100):
+    def __init__(self,
+                 categories,
+                 category_size,
+                 crossover_rate,
+                 mutation_rate,
+                 generation_size=100):
         '''
         @categories - list of category names (example: ['men', 'women'])
-        @items_per_category - number of elements in each of the categories,
+        @category_size - number of elements in each of the categories,
         i.e: number of matches to be made.
-        @generation_size - how many 'solutions' will there be for each
+        @generation_size - how many solutions will there be in each
         generation of the genetic algorithm.
+        @crossover_rate - probability of a crossover occuring
+        @mutation_rate - probability of a mutation occuring
         '''
         self.categories = categories
-        self.items_per_category = items_per_category
+        self.items_per_category = category_size
         self.generation_size = generation_size
+        self.crossover_rate = crossover_rate
+        self.mutation_rate = mutation_rate
 
     def run(self, seconds_to_run):
         '''
         @seconds_to_run - Number in seconds to run the algorithm
         '''
-        # Generate a pool of random possible solutions
-        self.solutions = \
+        # Generate a pool of random possible population
+        self.population = \
             [Solution(categories=self.categories,
                       categories_size=self.items_per_category)
              for _ in xrange(self.generation_size)]
@@ -39,10 +49,41 @@ class MatchMaker(object):
         elapsed = 0
         while (elapsed < seconds_to_run):
             # Iterate the algorithm
-            self.solutions = self.get_next_generation(self.solutions)
+            self.population = self.get_next_generation(self.population)
 
             # Count the running time
             elapsed = time.time() - start_time
 
-    def get_next_generation(self, solutions):
-        pass
+    def get_next_generation(self, population):
+
+        # Get the total fitness of the population
+        total_fitness = sum([solution.fitness
+                             for solution
+                             in population])
+
+        # Create a 'roulette wheel pool' of solutions
+        # to choose solutions that will contribute to the
+        # next generation
+        mating_pool = [solution * (solution.fitness / total_fitness)
+                       for solution
+                       in population]
+
+        next_generation = []
+
+        for _ in xrange(len(population)):
+            # Select randomly 2 solutions
+            first_solution = choice(mating_pool)
+            second_solution = choice(mating_pool)
+
+            # Crossover
+            if (random.random() < self.Pcrossover):
+                new_solution = first_solution.crossover(second_solution)
+            else:
+                new_solution = first_solution
+
+            # Mutate
+            if (random.random() < self.Pmutate):
+                new_solution.mutate()
+
+            # Add to the next generation
+            next_generation.append(new_solution)
